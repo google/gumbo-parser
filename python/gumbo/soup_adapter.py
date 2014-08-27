8c6ba9cfcff9095447cb6508e2238dea17d44cd8
Jonathan Tang <jonathan.d.tang@gmail.com>
2014-08-26 22:03:34 -0700

    Add the next/prev fields to BeautifulSoup adapter.

--- python/gumbo/soup_adapter.py
+++ python/gumbo/soup_adapter.py
@@ -22,6 +22,7 @@ can then manipulate like a normal BeautifulSoup parse tree.
 __author__ = 'jdtang@google.com (Jonathan Tang)'
 
 import BeautifulSoup
+import ctypes
 
 import gumboc
 
@@ -40,6 +41,16 @@ def _add_source_info(obj, original_text, start_pos, end_pos):
   obj.end_offset = end_pos.offset
 
 
+def _add_document(element):
+  # Currently ignored, since there's no real place for this in the BeautifulSoup
+  # API.
+  pass
+
+
+def _add_text(cls):
+  return lambda element: cls(_utf8(element.text))
+
+
 def _convert_attrs(attrs):
   # TODO(jdtang): Ideally attributes would pass along their positions as well,
   # but I can't extend the built in str objects with new attributes.  Maybe work
@@ -47,48 +58,66 @@ def _convert_attrs(attrs):
   return [(_utf8(attr.name), _utf8(attr.value)) for attr in attrs]
 
 
-def _add_document(soup, element):
-  # Currently ignored, since there's no real place for this in the BeautifulSoup
-  # API.
-  pass
+class _Converter(object):
+  def __init__(self, text, **kwargs):
+    # We need to record the addresses of GumboNodes as we add them and correlate
+    # them with the BeautifulSoup objects that they become.  This lets us
+    # correctly wire up the next/previous pointers so that they point to
+    # BeautifulSoup objects instead of ctypes ones.
+    self._node_map = {}
+    self._HANDLERS = [
+        _add_document,
+        self._add_element,
+        _add_text(BeautifulSoup.NavigableString),
+        _add_text(BeautifulSoup.CData),
+        _add_text(BeautifulSoup.Comment),
+        _add_text(BeautifulSoup.NavigableString),
+        ]
+    self.soup = BeautifulSoup.BeautifulSoup()
+    with gumboc.parse(text, **kwargs) as output:
+      self.soup.append(self._add_node(output.contents.root.contents))
+    
+    self._fix_next_prev_pointers(self.soup)
 
+  def _add_element(self, element):
+    tag = BeautifulSoup.Tag(
+        self.soup, _utf8(element.tag_name), _convert_attrs(element.attributes))
+    for child in element.children:
+      tag.append(self._add_node(child))
+    _add_source_info(
+        tag, element.original_tag, element.start_pos, element.end_pos)
+    tag.original_end_tag = str(element.original_end_tag)
+    return tag
 
-def _add_element(soup, element):
-  # TODO(jdtang): Expose next/previous in gumbo so they can be passed along to
-  # BeautifulSoup.
-  tag = BeautifulSoup.Tag(
-      soup, _utf8(element.tag_name), _convert_attrs(element.attributes))
-  for child in element.children:
-    tag.append(_add_node(soup, child))
-  _add_source_info(
-      tag, element.original_tag, element.start_pos, element.end_pos)
-  tag.original_end_tag = str(element.original_end_tag)
-  return tag
+  def _add_node(self, node):
+    result = self._HANDLERS[node.type.value](node.contents)
 
+    try:
+      result.next_addr = ctypes.addressof(node.next.contents)
+    except ValueError:
+      # Null pointer.
+      result.next_addr = 0
 
-def _add_text(cls):
-  def add_text_internal(soup, element):
-    text = cls(_utf8(element.text))
-    return text
-  return add_text_internal
+    try:
+      result.prev_addr = ctypes.addressof(node.prev.contents)
+    except ValueError:
+      # Null pointer.
+      result.prev_addr = 0
 
+    self._node_map[ctypes.addressof(node.contents)] = result
+    return result
 
-_HANDLERS = [
-    _add_document,
-    _add_element,
-    _add_text(BeautifulSoup.NavigableString),
-    _add_text(BeautifulSoup.CData),
-    _add_text(BeautifulSoup.Comment),
-    _add_text(BeautifulSoup.NavigableString),
-    ]
-
-
-def _add_node(soup, node):
-  return _HANDLERS[node.type.value](soup, node.contents)
+  def _fix_next_prev_pointers(self, tag):
+    tag.next = self._node_map.get(tag.next_addr)
+    tag.prev = self._node_map.get(tag.prev_addr)
+    try:
+      for child in tag.children:
+        self._fix_next_prev_pointers(child)
+    except (AttributeError, TypeError):
+      # NavigableStrings
+      pass
 
 
 def parse(text, **kwargs):
-  with gumboc.parse(text, **kwargs) as output:
-    soup = BeautifulSoup.BeautifulSoup()
-    soup.append(_add_node(soup, output.contents.root.contents))
-    return soup
+  converter = _Converter(text, **kwargs)
+  return converter.soup
