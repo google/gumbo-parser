bcad2794a06e3fbc5c7119a79c50e0d895223c3d
Kevin Hendricks <kevinhendricks@users.noreply.github.com>
2015-02-11 16:06:28 -0500

    Fixes to serialize to handle unknown tags

--- examples/serialize.cc
+++ examples/serialize.cc
@@ -15,7 +15,7 @@
 //
 // Author: Kevin Hendricks
 //
-// Serialize back to html / xhtml.
+// Serialize back to html / xhtml making as few changes as possible (even in whitespace)
 
 #include <fstream>
 #include <iostream>
@@ -24,16 +24,24 @@
 
 #include "gumbo.h"
 
-static std::string nonbreaking_inline  = "|a|abbr|acronym|b|bdo|big|cite|code|dfn|em|i|img|kbd|small|span|strike|strong|sub|sup|tt|";
-static std::string empty_tags          = "|br|hr|input|img|meta|spacer|link|frame|base|image|";
+static std::string nonbreaking_inline  = "|a|abbr|acronym|b|bdo|big|cite|code|dfn|em|font|i|img|kbd|nobr|s|small|span|strike|strong|sub|sup|tt|";
+static std::string empty_tags          = "|area|base|basefont|bgsound|br|command|col|embed|event-source|frame|hr|image|img|input|keygen|link|menuitem|meta|param|source|spacer|track|wbr|";
 static std::string preserve_whitespace = "|pre|textarea|script|style|";
 static std::string special_handling    = "|html|body|";
 static std::string no_entity_sub       = "|script|style|";
+
 
 static inline void rtrim(std::string &s) 
 {
   s.erase(s.find_last_not_of(" \n\r\t")+1);
 }
+
+
+static inline void ltrim(std::string &s)
+{
+  s.erase(0,s.find_first_not_of(" \n\r\t"));
+}
+
 
 static void replace_all(std::string &s, const char * s1, const char * s2)
 {
@@ -46,6 +54,7 @@ static void replace_all(std::string &s, const char * s1, const char * s2)
   }
 }
 
+
 static std::string substitute_xml_entities_into_text(const std::string &text)
 {
   std::string result = text;
@@ -55,6 +64,7 @@ static std::string substitute_xml_entities_into_text(const std::string &text)
   replace_all(result, ">", "&gt;");
   return result;
 }
+
 
 static std::string substitute_xml_entities_into_attributes(char quote, const std::string &text)
 {
@@ -68,14 +78,98 @@ static std::string substitute_xml_entities_into_attributes(char quote, const std
  return result;
 }
 
+
+static std::string handle_unknown_tag(GumboStringPiece *text)
+{
+  std::string tagname = "";
+  if (text->data == NULL) {
+    return tagname;
+  }
+  // work with copy GumboStringPiece to prevent asserts 
+  // if try to read same unknown tag name more than once
+  GumboStringPiece gsp = *text;
+  gumbo_tag_from_original_text(&gsp);
+  tagname = std::string(gsp.data, gsp.length);
+  return tagname; 
+}
+
+
+static std::string get_tag_name(GumboNode *node)
+{
+  std::string tagname;
+  // work around lack of proper name for document node
+  if (node->type == GUMBO_NODE_DOCUMENT) {
+    tagname = "document";
+  } else {
+    tagname = gumbo_normalized_tagname(node->v.element.tag);
+  }
+  if (tagname.empty()) {
+    tagname = handle_unknown_tag(&node->v.element.original_tag);
+  }
+  return tagname;
+}
+
+
+static std::string build_doctype(GumboNode *node)
+{
+  std::string results = "";
+  if (node->v.document.has_doctype) {
+    results.append("<!DOCTYPE ");
+    results.append(node->v.document.name);
+    std::string pi(node->v.document.public_identifier);
+    if ((node->v.document.public_identifier != NULL) && !pi.empty() ) {
+        results.append(" PUBLIC \"");
+        results.append(node->v.document.public_identifier);
+        results.append("\" \"");
+        results.append(node->v.document.system_identifier);
+        results.append("\"");
+    }
+    results.append(">\n");
+  }
+  return results;
+}
+
+
+static std::string build_attributes(GumboAttribute * at, bool no_entities)
+{
+  std::string atts = " ";
+  atts.append(at->name);
+
+  // how do we want to handle attributes with empty values
+  // <input type="checkbox" checked />  or <input type="checkbox" checked="" /> 
+
+  if ( (!std::string(at->value).empty())   || 
+       (at->original_value.data[0] == '"') || 
+       (at->original_value.data[0] == '\'') ) {
+
+    // determine original quote character used if it exists
+    char quote = at->original_value.data[0];
+    std::string qs = "";
+    if (quote == '\'') qs = std::string("'");
+    if (quote == '"') qs = std::string("\"");
+    atts.append("=");
+    atts.append(qs);
+    if (no_entities) {
+      atts.append(at->value);
+    } else {
+      atts.append(substitute_xml_entities_into_attributes(quote, std::string(at->value)));
+    }
+    atts.append(qs);
+  }
+  return atts;
+}
+
+
 // forward declaration
 static std::string serialize(GumboNode*);
 
+
 // serialize children of a node
 // may be invoked recursively
+
 static std::string serialize_contents(GumboNode* node) {
   std::string contents        = "";
-  std::string tagname         = gumbo_normalized_tagname(node->v.element.tag);
+  std::string tagname         = get_tag_name(node);
   std::string key             = "|" + tagname + "|";
   bool no_entity_substitution = no_entity_sub.find(key) != std::string::npos;
   bool keep_whitespace        = preserve_whitespace.find(key) != std::string::npos;
@@ -83,20 +177,24 @@ static std::string serialize_contents(GumboNode* node) {
 
   // build up result for each child, recursively if need be
   GumboVector* children = &node->v.element.children;
+
   for (unsigned int i = 0; i < children->length; ++i) {
     GumboNode* child = static_cast<GumboNode*> (children->data[i]);
+
     if (child->type == GUMBO_NODE_TEXT) {
       if (no_entity_substitution) {
         contents.append(std::string(child->v.text.text));
       } else {
         contents.append(substitute_xml_entities_into_text(std::string(child->v.text.text)));
       }
+
     } else if (child->type == GUMBO_NODE_ELEMENT) {
       contents.append(serialize(child));
+
     } else if (child->type == GUMBO_NODE_WHITESPACE) {
-      if (keep_whitespace or is_inline) {
-        contents.append(std::string(child->v.text.text));
-      }
+      // keep all whitespace to keep as close to original as possible
+      contents.append(std::string(child->v.text.text));
+
     } else if (child->type != GUMBO_NODE_COMMENT) {
       // Does this actually exist: (child->type == GUMBO_NODE_CDATA)
       fprintf(stderr, "unknown element of type: %d\n", child->type); 
@@ -105,34 +203,22 @@ static std::string serialize_contents(GumboNode* node) {
   return contents;
 }
 
+
 // serialize a GumboNode back to html/xhtml
 // may be invoked recursively
-static std::string serialize(GumboNode* node) {
-  std::string close = "";
-  std::string closeTag = "";
-  std::string atts = "";
 
+static std::string serialize(GumboNode* node) {
   // special case the document node
   if (node->type == GUMBO_NODE_DOCUMENT) {
-    std::string results = "";
-    if (node->v.document.has_doctype) {
-      results.append("<!DOCTYPE ");
-      results.append(node->v.document.name);
-      std::string pi(node->v.document.public_identifier);
-      if ((node->v.document.public_identifier != NULL) && !pi.empty() ) {
-          results.append(" PUBLIC \"");
-          results.append(node->v.document.public_identifier);
-          results.append("\" \"");
-          results.append(node->v.document.system_identifier);
-          results.append("\"");
-      }
-      results.append(">\n");
-    }
+    std::string results = build_doctype(node);
     results.append(serialize_contents(node));
     return results;
   }
 
-  std::string tagname            = gumbo_normalized_tagname(node->v.element.tag);
+  std::string close = "";
+  std::string closeTag = "";
+  std::string atts = "";
+  std::string tagname            = get_tag_name(node);
   std::string key                = "|" + tagname + "|";
   bool need_special_handling     =  special_handling.find(key) != std::string::npos;
   bool is_empty_tag              = empty_tags.find(key) != std::string::npos;
@@ -143,27 +229,7 @@ static std::string serialize(GumboNode* node) {
   const GumboVector * attribs = &node->v.element.attributes;
   for (int i=0; i< attribs->length; ++i) {
     GumboAttribute* at = static_cast<GumboAttribute*>(attribs->data[i]);
-    atts.append(" ");
-    atts.append(at->name);
-    // how do we want to handle attributes with empty values
-    // <input type="checkbox" checked />  or 
-    // <input type="checkbox" checked="" /> 
-    if ( (!std::string(at->value).empty())   || 
-         (at->original_value.data[0] == '"') || 
-         (at->original_value.data[0] == '\'') ) {
-      char quote = at->original_value.data[0];
-      std::string qs = "";
-      if (quote == '\'') qs = std::string("'");
-      if (quote == '"') qs = std::string("\"");
-      atts.append("=");
-      atts.append(qs);
-      if (no_entity_substitution) {
-        atts.append(at->value);
-      } else {
-        atts.append(substitute_xml_entities_into_attributes(quote, std::string(at->value)));
-      }
-      atts.append(qs);
-    }
+    atts.append(build_attributes(at, no_entity_substitution));
   }
 
   // determine closing tag type
@@ -173,9 +239,11 @@ static std::string serialize(GumboNode* node) {
       closeTag = "</" + tagname + ">";
   }
 
-  // serialize any children
+  // serialize your contents
   std::string contents = serialize_contents(node);
+
   if (need_special_handling) {
+    ltrim(contents);
     rtrim(contents);
     contents.append("\n");
   }
@@ -186,15 +254,14 @@ static std::string serialize(GumboNode* node) {
   if (need_special_handling) results.append("\n");
   results.append(contents);
   results.append(closeTag);
-  if (! is_inline) results.append("\n");
-
+  if (need_special_handling) results.append("\n");
   return results;
 }
 
 
 int main(int argc, char** argv) {
   if (argc != 2) {
-      std::cout << "clean_html <html filename>\n";
+      std::cout << "serialize <html filename>\n";
       exit(EXIT_FAILURE);
   }
   const char* filename = argv[1];
@@ -211,8 +278,11 @@ int main(int argc, char** argv) {
   in.seekg(0, std::ios::beg);
   in.read(&contents[0], contents.size());
   in.close();
+ 
+  GumboOptions myoptions = kGumboDefaultOptions;
+  myoptions.use_xhtml_rules = true;
 
-  GumboOutput* output = gumbo_parse(contents.c_str());
+  GumboOutput* output = gumbo_parse_with_options(&myoptions, contents.data(), contents.length());
   std::cout << serialize(output->document) << std::endl;
   gumbo_destroy_output(&kGumboDefaultOptions, output);
 }
