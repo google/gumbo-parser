b7c321a4253fc858e27ac904f8d0dcb13b709010
Jonathan Tang <jonathan.d.tang@gmail.com>
2015-03-22 00:03:59 -0700

    Test for both glibtoolize and libtoolize on Mac; the downloaded version of autotools only includes libtoolize.

--- autogen.sh
+++ autogen.sh
@@ -15,7 +15,14 @@
 # OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
 if test -z "$LIBTOOLIZE" -a "`uname`" = "Darwin"; then
-  LIBTOOLIZE=glibtoolize
+	if command -v "glibtoolize" >/dev/null; then
+		LIBTOOLIZE=glibtoolize
+	elif command -v "libtoolize" >/dev/null; then
+		LIBTOOLIZE=libtoolize
+	else
+		echo "autogen.sh: line $LINENO: command glibtoolize or libtoolize not found"
+		exit 1
+	fi
 fi
 
 set -ex
